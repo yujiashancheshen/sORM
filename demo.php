5a9c74f0670a6d611a3a960d8a01522edec7708f
汪卓 <wangzhuo@didichuxing.com>
2018-01-01 14:44:12 +0800

    增加脚手架功能

--- demo.php
+++ demo.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 1);
-error_reporting(E_ERROR | E_WARNING);
+//error_reporting(E_ERROR | E_WARNING);
+error_reporting(E_ALL);
 
 require_once __DIR__ . "/model/UserModel.php";
 require_once __DIR__ . "/orm/db.php";
--- model/UserModel.php
+++ model/UserModel.php
@@ -1,15 +1,15 @@
 <?php
 require_once __DIR__ . "/../orm/model.php";
 
-class UserModel extends model {
+class UserModel extends Model {
 
     public static $table = 'user';
+
+    public static $primaryKey = 'id';
 
     public static $fields = [
         'id' => 'id',
         'name' => 'name',
-        'age' => 'age'
+        'age' => 'age',
     ];
-
-    public static $primaryKey = 'id';
 }
